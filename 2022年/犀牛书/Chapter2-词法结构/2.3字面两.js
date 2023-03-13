ef66b9da614311f1a26783bbd97e28856bf5044a
vccyb <13170906656@163.com>
2023-03-13 23:46:09 +0800

    reading chapter2 词法结构

---
+++ 2022年/犀牛书/Chapter2-词法结构/2.3字面两.js
@@ -0,0 +1,8 @@
+let a = 12
+let b = 1.2
+let c = "hello world"
+let d = "Hi"
+let e = true
+let f = false
+let g = null
+
---
+++ 2022年/犀牛书/Chapter2-词法结构/2.4标识符和保留字.js
@@ -0,0 +1,12 @@
+
+
+let i;
+let my_xxx
+let v13
+let _dummy
+let $str
+
+//  字母 下划线 $ 开头
+// 身体可以 字母 下划线 $ 数字
+
+// 保留字很多 let const if of ...
