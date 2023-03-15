19c95979bf0c1f6e8f94111ef48733599b77b74f
vccyb <13170906656@163.com>
2023-03-15 22:59:06 +0800

    3-15

--- 2022年/犀牛书/Chapter3-类型、值和变量/3.2数值.js
+++ 2022年/犀牛书/Chapter3-类型、值和变量/3.2数值.js
@@ -4,3 +4,14 @@ let b = 0xff
 console.log(b)
 
 let billion = 1_000_000_000
+
+
+// 3.5 null 和 undefined
+// 3.6 symbol
+let s = Symbol('sym_x')
+console.log(s.toString());
+
+
+let [x, y] = [1, 2]
+[x, y] = [x + 1, y + 1]
+[x, y] = [y, x];
