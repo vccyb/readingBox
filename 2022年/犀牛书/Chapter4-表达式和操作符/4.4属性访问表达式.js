e546791cd905cf8ea639c7e7b1b064878f0c5b7a
vccyb <13170906656@163.com>
2023-03-16 23:33:03 +0800

    3-16

---
+++ 2022年/犀牛书/Chapter4-表达式和操作符/4.4属性访问表达式.js
@@ -0,0 +1,20 @@
+let a = [4, [5, 6]]
+
+
+// ?. 和 ?.[]  更加好用
+
+
+function square(x, log) {
+  log?.(x);
+  return
+}
+  
+
+  //  o.m()
+  // o?.m()
+  // o.m?.()
+
+
+let data = [7,8,9];
+
+console.log(3 in data)
