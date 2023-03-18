353fccfa435056dd8c63144888390ae578737702
vccyb <13170906656@163.com>
2023-03-18 23:06:50 +0800

    3-18

---
+++ 2022年/犀牛书/Chapter9-类/index.js
@@ -0,0 +1,58 @@
+
+// class EZArray extends Array {
+//   get first() {
+//     return this[0]
+//   }
+
+//   get last() {
+//     return this[this.length - 1]
+//   }
+// }
+
+
+// let a = new EZArray()
+
+// a.push(1,2,3,4)
+
+// console.log(a.first);
+// console.log(a.last);
+
+// console.log(Array.isArray(a));
+// console.log(EZArray.isArray(a));
+
+
+// node 
+
+// const square = x => x * x;
+// const sum = (x, y) => x + y;
+
+// exports.mean = data => data.xxx
+// exports.stddev = function(d) {
+//   xxx
+// }
+
+
+// module.exports = class BitSet extends xxx {
+
+// }
+
+// module.exports = {xxx, xxx}
+
+
+// const fs = require("fs")
+// const http = require("http")
+
+// const express = require("express")
+
+// const stats = require("./xxx")
+
+
+
+// es6
+
+
+
+// import()
+
+// await import ("./xxxjs")
+
