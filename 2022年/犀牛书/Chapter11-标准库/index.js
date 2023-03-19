4fb7c57850e5f0aea93eb9fa9c924cf49d703ac4
vccyb <13170906656@163.com>
2023-03-19 20:23:36 +0800

    3-16

---
+++ 2022年/犀牛书/Chapter11-标准库/index.js
@@ -0,0 +1,23 @@
+
+
+// set
+
+// let s = new Set()
+
+// let t = new Set([1, s])
+
+// let t = new Set(s);
+// let unique = new Set("Mississippi")
+
+// set.add
+
+
+// // map
+// let o = {x: 1, y:2}
+// let copy = new Map(Object.entries(o))
+// map.set
+
+
+// reg
+
+let pattern = /s$/i
