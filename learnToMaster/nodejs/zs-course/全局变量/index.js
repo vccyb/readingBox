c91e4acdc88fb2d442280cf35c3e6b6ed71773ff
vccyb <13170906656@163.com>
2024-01-23 22:45:22 +0800

    1-23-study-3

--- learnToMaster/nodejs/zs-course/全局变量/index.js
+++ learnToMaster/nodejs/zs-course/全局变量/index.js
@@ -1,3 +1,34 @@
 global.name = "xxx";
 
-console.log(global);
+// console.log(global);
+
+// globalThis
+// node 没有 DOM 和 BOM
+
+// node环境内置的一些 api
+// dirname 当前文件的文件目录，执行脚本的目录，绝对路径
+// filename 文件名字
+// extname 后缀
+
+console.log(__dirname);
+console.log(__filename);
+// console.log(extname);
+
+// Buffer
+
+// process
+
+console.log(process.argv);
+console.log(process.cwd());
+
+setTimeout(() => {
+  console.log("结束");
+}, 5000);
+
+setTimeout(() => {
+  process.exit();
+}, 1000);
+
+process.on("exit", (code) => {
+  console.log("退出");
+});
