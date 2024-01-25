b7c22191c2dfe8a578e5532c5be156803b120296
vccyb <13170906656@163.com>
2024-01-25 22:01:40 +0800

    1-25-study

--- learnToMaster/nodejs/zs-course/全局变量/index.js
+++ learnToMaster/nodejs/zs-course/全局变量/index.js
@@ -1,34 +1,48 @@
-global.name = "xxx";
+// global.name = "xxx";
 
-// console.log(global);
+// // console.log(global);
 
-// globalThis
-// node 没有 DOM 和 BOM
+// // globalThis
+// // node 没有 DOM 和 BOM
 
-// node环境内置的一些 api
-// dirname 当前文件的文件目录，执行脚本的目录，绝对路径
-// filename 文件名字
-// extname 后缀
+// // node环境内置的一些 api
+// // dirname 当前文件的文件目录，执行脚本的目录，绝对路径
+// // filename 文件名字
+// // extname 后缀
 
-console.log(__dirname);
-console.log(__filename);
-// console.log(extname);
+// console.log(__dirname);
+// console.log(__filename);
+// // console.log(extname);
 
-// Buffer
+// // Buffer
 
-// process
+// // process
 
-console.log(process.argv);
-console.log(process.cwd());
+// console.log(process.argv);
+// console.log(process.cwd());
 
-setTimeout(() => {
-  console.log("结束");
-}, 5000);
+// setTimeout(() => {
+//   console.log("结束");
+// }, 5000);
 
-setTimeout(() => {
-  process.exit();
-}, 1000);
+// setTimeout(() => {
+//   process.exit();
+// }, 1000);
 
-process.on("exit", (code) => {
-  console.log("退出");
-});
+// process.on("exit", (code) => {
+//   console.log("退出");
+// });
+
+// // kill 杀死进程
+// // process.kill(process.pid);
+
+// // env 环境变量
+// // 获取操作系统所有的环境变量
+// 可以修改，但是 修改只在当前进程生效
+process.env.name = "xxx";
+console.log(process.env);
+
+// cross-env
+// 跨平台
+
+// windows set posix export
