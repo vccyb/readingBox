03adaef82a07fd1b32b399317af2a7f8cb6f9ee9
vccyb <13170906656@163.com>
2024-03-09 14:46:33 +0800

    update promise2

--- 源码系列/TDD/MyPromise2/APromise.js
+++ 源码系列/TDD/MyPromise2/APromise.js
@@ -16,6 +16,7 @@ class APromise {
     if (this.PromiseState === APromise.PENDING) {
       this.PromiseState = APromise.FULFILLED;
       this.PromiseResult = value;
+      // 先把本次同步的执行完，resolve的之后在执
       queueMicrotask(() => {
         this.callbacks.map((callback) => {
           callback.onFulfilled(value);
@@ -28,7 +29,7 @@ class APromise {
       this.PromiseState = APromise.REJECTED;
       this.PromiseResult = reason;
       queueMicrotask(() => {
-        this.callback.map((callback) => {
+        this.callbacks.map((callback) => {
           callback.onRejected(reason);
         });
       });
@@ -44,45 +45,62 @@ class APromise {
         : (reason) => {
             throw reason;
           };
-    if (this.PromiseState === APromise.PENDING) {
-      this.callbacks.push({
-        onFulfilled,
-        onRejected,
-      });
-    }
-    if (this.PromiseState === APromise.FULFILLED) {
-      // 下一次执行
-      queueMicrotask(() => {
-        onFulfilled(this.PromiseResult);
-      });
-    }
 
-    if (this.PromiseState === APromise.REJECTED) {
-      // 下一次执行
-      queueMicrotask(() => {
-        onRejected(this.PromiseResult);
-      });
+    const promise2 = new APromise((resolve, reject) => {
+      if (this.PromiseState === APromise.PENDING) {
+        this.callbacks.push({
+          onFulfilled: (value) => {
+            this.parse(promise2, onFulfilled(value), resolve, reject);
+          },
+          onRejected: (value) => {
+            this.parse(promise2, onRejected(value), resolve, reject);
+          },
+        });
+      }
+      if (this.PromiseState === APromise.FULFILLED) {
+        // 下一次执行
+        queueMicrotask(() => {
+          this.parse(
+            promise2,
+            onFulfilled(this.PromiseResult),
+            resolve,
+            reject
+          );
+        });
+      }
+
+      if (this.PromiseState === APromise.REJECTED) {
+        // 下一次执行
+        queueMicrotask(() => {
+          this.parse(promise2, onRejected(this.PromiseResult), resolve, reject);
+        });
+      }
+    });
+    return promise2;
+  }
+
+  parse(promise2, result, resolve, reject) {
+    if (promise2 === result) {
+      throw new TypeError("Chaining cycle detected for promise");
+    }
+    try {
+      if (result instanceof APromise) {
+        result.then(resolve, reject);
+      } else {
+        resolve(result);
+      }
+    } catch (e) {
+      reject(e);
     }
   }
 }
 
-console.log(1);
-let promise1 = new APromise((resolve, reject) => {
-  console.log(2);
-  setTimeout(() => {
-    console.log("A", promise1.PromiseState);
-    resolve("这次一定");
-    console.log("B", promise1.PromiseState);
-    console.log(4);
-  });
+let promise = new APromise((resolve, reject) => {
+  resolve("123");
 });
-promise1.then(
-  (result) => {
-    console.log("C", promise1.PromiseState);
-    console.log("fulfilled:", result);
-  },
-  (reason) => {
-    console.log("rejected:", reason);
-  }
-);
-console.log(3);
+
+let p = promise.then((value) => {
+  return p;
+});
+
+console.log(p);
