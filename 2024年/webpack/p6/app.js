c8d120e510cf19d73a813c2f1a8262abfd3662ba
vccyb <13170906656@163.com>
2024-04-19 00:21:07 +0800

    webpack 01

---
+++ 2024年/webpack/p6/app.js
@@ -0,0 +1,7 @@
+// import { fooName } from "./module.js";
+// import { default as qqNAme } from "./module.js";
+// console.log(qqNAme);
+// console.log(fooName);
+
+import zzName from "./module.js";
+console.log(zzName);
---
+++ 2024年/webpack/p6/module.js
@@ -0,0 +1,14 @@
+// export var name = "foo module";
+
+// export function sayHello() {
+//   console.log("Hello from foo module!");
+// }
+
+// export class Person {}
+
+var name = "foo module";
+
+var qq = "123456";
+var zz = "zzzz";
+// export { name as fooName, qq as default };
+export default zz;
